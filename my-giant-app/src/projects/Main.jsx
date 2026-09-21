c79c5da5bad1b079e0067dbaceb7ab87226c6627
pascojoseph012-masta <pascojoseph012@gmail.com>
2026-09-21 10:16:56 +0200

    initial commit of deplyment

--- my-giant-app/src/projects/Main.jsx
+++ my-giant-app/src/projects/Main.jsx
@@ -1,7 +1,9 @@
 import React from "react";
+import {Link} from "react-router-dom"
 import logo from "../assets/vite.svg";
 import smbars from "../assets/smbars.svg";
 import image1 from "../assets/first-image.png"
+import image2 from "../assets/second-image.png"
 import slide1 from "../assets/10008.png"
 import slide2 from "../assets/10009.png"
 import slide3 from "../assets/10010.png"
@@ -39,11 +41,11 @@ const Main = () => {
           
           ">
 
-            <a href="#" className="min-[20px]:hidden min-[1024px]:block lg:hover:border-b-3 lg:rounded-md lg:active:border-b-2 lg:active:rounded-2xl lg:transiton lg:duration-100 lg:linear lg:border-white lg:h-8 lg:w-15 lg:text-sm lg:flex items-center lg:justify-center  lg:font-bold ">Home</a>
-            <a href="#" className="min-[20px]:hidden min-[1024px]:block lg:hover:border-b-3 lg:rounded-md lg:active:border-b-2 lg:active:rounded-2xl lg:transiton lg:duration-100 lg:linear lg:border-white lg:h-8 lg:w-15 lg:text-sm lg:flex items-center lg:justify-center  lg:font-bold ">Service</a>
-            <a href="#" className="min-[20px]:hidden min-[1024px]:block lg:hover:border-b-3 lg:rounded-md lg:active:border-b-2 lg:active:rounded-2xl lg:transiton lg:duration-100 lg:linear lg:border-white lg:h-8 lg:w-18 lg:text-sm lg:flex items-center lg:justify-center  lg:font-bold ">Our Work</a>
-            <a href="#" className="min-[20px]:hidden min-[1024px]:block lg:hover:border-b-3 lg:rounded-md lg:active:border-b-2 lg:active:rounded-2xl lg:transiton lg:duration-100 lg:linear lg:border-white lg:h-8 lg:w-18 lg:text-sm lg:flex items-center lg:justify-center  lg:font-bold ">About Us</a>
-            <a href="#" className="min-[20px]:hidden min-[1024px]:block lg:hover:border-b-3 lg:rounded-md lg:active:border-b-2 lg:active:rounded-2xl lg:transiton lg:duration-100 lg:linear lg:border-white lg:h-8 lg:w-15 lg:text-sm lg:flex items-center lg:justify-center  lg:font-bold ">Contact</a>
+            <Link to="#" className="min-[20px]:hidden min-[1024px]:block lg:hover:border-b-3 lg:rounded-md lg:active:border-b-2 lg:active:rounded-2xl lg:transiton lg:duration-100 lg:linear lg:border-white lg:h-8 lg:w-15 lg:text-sm lg:flex items-center lg:justify-center  lg:font-bold ">Home</Link>
+            <Link to="#" className="min-[20px]:hidden min-[1024px]:block lg:hover:border-b-3 lg:rounded-md lg:active:border-b-2 lg:active:rounded-2xl lg:transiton lg:duration-100 lg:linear lg:border-white lg:h-8 lg:w-15 lg:text-sm lg:flex items-center lg:justify-center  lg:font-bold ">Service</Link>
+            <Link to="#" className="min-[20px]:hidden min-[1024px]:block lg:hover:border-b-3 lg:rounded-md lg:active:border-b-2 lg:active:rounded-2xl lg:transiton lg:duration-100 lg:linear lg:border-white lg:h-8 lg:w-18 lg:text-sm lg:flex items-center lg:justify-center  lg:font-bold ">Our Work</Link>
+            <Link to="#" className="min-[20px]:hidden min-[1024px]:block lg:hover:border-b-3 lg:rounded-md lg:active:border-b-2 lg:active:rounded-2xl lg:transiton lg:duration-100 lg:linear lg:border-white lg:h-8 lg:w-18 lg:text-sm lg:flex items-center lg:justify-center  lg:font-bold ">About Us</Link>
+            <Link to="#" className="min-[20px]:hidden min-[1024px]:block lg:hover:border-b-3 lg:rounded-md lg:active:border-b-2 lg:active:rounded-2xl lg:transiton lg:duration-100 lg:linear lg:border-white lg:h-8 lg:w-15 lg:text-sm lg:flex items-center lg:justify-center  lg:font-bold ">Contact</Link>
 
           </div>
           <button type="button" className="min-[20px]:hidden lg:block lg:w-40 lg:cursor-pointer  lg:rounded-sm lg:h-10 lg:border lg:border-[#ec711e] lg:text-[14px]">LET's TALk  <span clasName="">&#8594;</span> </button>
@@ -67,7 +69,7 @@ const Main = () => {
               </h1>
 
               <div className="w-full h-40  flex flex-col items-start gap-5 md:flex md:flex-row  md:h-15 sm:flex sm:flex-row  sm:h-15 lg:w-100">
-                <button type="button" className=" cursor-pointer w-full text-white text-xl font-semibold h-12 rounded-md backdrop-blur-xl bg-linear-to-r from-[#eb640aa6] to-[#ec403b] hover:bg-[#f16d14a6] ">Our Service &#8594;</button>
+                <button type="button" className=" cursor-pointer w-full text-white text-xl font-semibold h-12 rounded-md backdrop-blur-xl bg-linear-to-r from-[#eb640aa6] to-[#ec403b] hover:bg-[#f16d14a6] ">LET'S TALK &#8594;</button>
                 <button type="button" className=" cursor-pointer w-full text-white text-xl font-semibold h-12 rounded-md  border border-white md:border-2 md:backdrop-blur-md">View Our Work</button>
               </div>
             
@@ -134,7 +136,7 @@ const Main = () => {
           <div className="w-full h-480 bg-slate flex flex-col px-3 2xl:px-15 mt-15 sm:h-270 md:h-270 border-b border-zinc-800">
             <div className="h-55 bg-amber flex flex-col items-center justify-start gap-2">
                <p className="text-lg text-[#ec711e] font-bold">OUR WORK</p>
-              <h1 className="text-5xl text-white font-bold">Featured Projects</h1>
+              <h1 className="text-5xl text-white font-bold text-center">Featured Projects</h1>
               <h1 className="text-center text-2sm sm:w-100">Explore some of our recent work and see how we turn ideas into powerfull visual stories</h1>
             </div>
             {/* card work section */}
@@ -143,47 +145,47 @@ const Main = () => {
             xl:grid xl:grid-cols-2 xl:items-center xl:justify-start 2xl:grid 2xl:grid-cols-2 2xl:items-center 2xl:justify-start
             ">
               {/* card */}
-              <div className=" border bg-[#0f1011] border-zinc-800 flex flex-col h-100 w-full  ">
-                <div className="w-full h-85 "> <img src={photo} className="w-full h-75 object-cover" /> </div>
+              <div className=" border bg-[#0f1011] group border-zinc-800 flex flex-col h-100 w-full  ">
+                <div className="w-full h-85 overflow-hidden "> <img src={photo} className="w-full h-75 object-cover group-hover:scale-105 transition duration-500 ease-in-out " /> </div>
                 <div className="w-full h-30 bg-amber flex flex-row items-end justify-around lg:justify-normal px-2 py-2">
                   <div className="border-l-5 border-[#ec711e] h-20 bg-amber justify-around lg:w-150 xl:w-150 px-2 flex flex-col">
                     <p className="text-white text-xl font-bold">Photo Editing</p>
                     <h1 className="font-bold text-[15px]">Retouching . Manipulation . Enhencment</h1>
                   </div>
-                    <button type="button" className="w-10 h-10 rounded-full relative bottom-5 border border-[#ec711e] text-2xl cursor-pointer flex itmes-center justify-center">&#8594;</button>
+                    <button type="button" className="w-10 h-10 group-hover:animate-bounce rounded-full relative bottom-5 border border-[#ec711e] text-2xl cursor-pointer flex itmes-center justify-center">&#8594;</button>
                 </div>
               </div>
 
-              <div className=" border bg-[#0f1011] border-zinc-800 flex flex-col h-100 w-full">
-                <div className="w-full h-85 "> <img src={photo1} className="w-full h-75 object-cover" /> </div>
+              <div className=" border bg-[#0f1011] group border-zinc-800 flex flex-col h-100 w-full">
+                <div className="w-full h-85 overflow-hidden"> <img src={photo1} className="w-full h-75 object-cover group-hover:scale-105 transition duration-300 ease-in-out" /> </div>
                 <div className="w-full h-30 bg-amber flex flex-row items-end justify-  px-2 py-2">
                   <div className="border-l-5 border-[#ec711e] h-20 bg-amber justify-around w-75 lg:w-150 xl:w-150  px-2 flex flex-col">
                     <p className="text-white text-xl font-bold">Graphic Design</p>
                     <h1 className="font-bold text-[15px]">Branding . Print . Digital</h1>
                   </div>
-                    <button type="button" className="w-10 h-10 rounded-full relative bottom-5 border border-[#ec711e] text-2xl cursor-pointer flex itmes-center justify-center">&#8594;</button>
+                    <button type="button" className="w-10 h-10 rounded-full group-hover:animate-bounce relative bottom-5 border border-[#ec711e] text-2xl cursor-pointer flex itmes-center justify-center">&#8594;</button>
                 </div>
               </div>
 
-              <div className=" border bg-[#0f1011] border-zinc-800 flex flex-col h-100 w-full">
-                <div className="w-full h-85 "> <img src={photo3} className="w-full h-75 object-cover" /> </div>
+              <div className=" border bg-[#0f1011] group border-zinc-800 flex flex-col h-100 w-full">
+                <div className="w-full h-85 overflow-hidden"> <img src={photo3} className="w-full h-75 object-cover group-hover:scale-105 transition duration-300 ease-in-out" /> </div>
                 <div className="w-full h-30 bg-amber flex flex-row items-end justify-  px-2 py-2">
                   <div className="border-l-5 border-[#ec711e] h-20 bg-amber justify-around w-75 lg:w-150 xl:w-150  px-2 flex flex-col">
                     <p className="text-white text-xl font-bold">Social Media Design</p>
                     <h1 className="font-bold text-[15px]">Web,dev . Ads . Stories</h1>
                   </div>
-                    <button type="button" className="w-10 h-10 rounded-full relative bottom-5 border border-[#ec711e] text-2xl cursor-pointer flex itmes-center justify-center">&#8594;</button>
+                    <button type="button" className="w-10 h-10 rounded-full group-hover:animate-bounce relative bottom-5 border border-[#ec711e] text-2xl cursor-pointer flex itmes-center justify-center">&#8594;</button>
                 </div>
               </div>
 
-              <div className=" border bg-[#0f1011] border-zinc-800 flex flex-col h-100 w-full">
-                <div className="w-full h-85 "> <img src={photo2} className="w-full h-75 object-cover" /> </div>
+              <div className=" border bg-[#0f1011] group border-zinc-800 flex flex-col h-100 w-full">
+                <div className="w-full h-85 overflow-hidden"> <img src={photo2} className="w-full h-75 object-cover group-hover:scale-105 transition duration-300 ease-in-out" /> </div>
                 <div className="w-full h-30 bg-amber flex flex-row items-end justify-  px-2 py-2">
                   <div className="border-l-5 border-[#ec711e] h-20 bg-amber justify-around w-75 lg:w-150 xl:w-150 px-2 flex flex-col">
                     <p className="text-white text-xl font-bold">Social Media Design</p>
                     <h1 className="font-bold text-[15px]">Web,dev . Ads . Stories</h1>
                   </div>
-                    <button type="button" className="w-10 h-10 rounded-full relative bottom-5 border border-[#ec711e] text-2xl cursor-pointer flex itmes-center justify-center">&#8594;</button>
+                    <button type="button" className="w-10 h-10 rounded-full group-hover:animate-bounce relative bottom-5 border border-[#ec711e] text-2xl cursor-pointer flex itmes-center justify-center">&#8594;</button>
                 </div>
               </div>
 
@@ -193,7 +195,7 @@ const Main = () => {
           </div>
           {/* why section continue */}
           <div className="w-full h-200 border-b border-zinc-800 px-3 flex flex-col gap-5 mt-4 md:grid md:grid-cols-2 sm:grid sm:grid-cols-2
-          lg:flex lg:flex-row lg:gap-2 lg:h-60 lg:mt-0
+          lg:flex lg:flex-row lg:gap-2 lg:h-60 lg:mt-0 lg:items-center lg:justify-center
           ">
             <div className="h-50 bg-amber flex flex-col items-center justify-evenly lg:border-none sm:border-b rounded-4xl sm:border-zinc-800">
                <p className="text-md font-bold text-[#ec711e]">WHY CHOOSE GIANTS?</p>
@@ -201,7 +203,7 @@ const Main = () => {
                <h1></h1>
             </div>
             
-              <div className="h-50 flex flex-col border-r-2 border-zinc-800 sm:border-r sm:border-zinc-800 lg:py-5 lg:h-30 lg:items-center lg:justify-center lg:mt-5 ">
+              <div className="h-50 flex flex-col border-r-2 border-zinc-800 sm:border-r sm:border-zinc-800 lg:py-5 lg:h-30 lg:items-center lg:justify-center  ">
                  <div className="w-full h-40  flex items-start justify-start px-10 py-2"><p className="text-5xl">🎈</p></div>
                  <div className="w-full h-full  flex flex-col items-start justify-start px-10 py-2">
                    <p className="text-white text-xl font-bold">Creative Excellence</p>
@@ -210,7 +212,7 @@ const Main = () => {
 
               </div>
 
-              <div className="h-50 flex flex-col border-r-2 border-zinc-800 sm:border-r sm:border-zinc-800 lg:py-5 lg:h-30 lg:items-center lg:justify-center lg:mt-5">
+              <div className="h-50 flex flex-col border-r-2 border-zinc-800 sm:border-r sm:border-zinc-800 lg:py-5 lg:h-30 lg:items-center lg:justify-center">
                  <div className="w-full h-40  flex items-start justify-start px-10 py-2"><p className="text-5xl">🎃</p></div>
                  <div className="w-full h-full  flex flex-col items-start justify-start px-10 py-2">
                    <p className="text-white text-xl font-bold">Experienced Team</p>
@@ -220,7 +222,7 @@ const Main = () => {
               </div>
 
               
-              <div className="h-50 flex flex-col border-r-2 border-zinc-800 sm:border-r sm:border-zinc-800 lg:border-none lg:py-5 lg:h-30 lg:items-center lg:justify-center lg:mt-5">
+              <div className="h-50 flex flex-col border-r-2 border-zinc-800 sm:border-r sm:border-zinc-800 lg:border-none lg:py-5 lg:h-30 lg:items-center lg:justify-center">
                  <div className="w-full h-40  flex items-start justify-start px-10 py-2"><p className="text-5xl">🎡</p></div>
                  <div className="w-full h-full  flex flex-col items-start justify-start px-10 py-2">
                    <p className="text-white text-xl font-bold">On-time Derivery</p>
@@ -233,7 +235,46 @@ const Main = () => {
 
           </div>
           {/* why section end */}
-          lodfghjkl
+          {/* last section start */}
+          <div className="w-full h-80 flex flex-col items-center justify-evenly px-3 border-b-2 border-zinc-800 "> 
+            <img src={image2} className="w-full h-80 object-cover -z-10 absolute opacity-50 " />
+              <p className="text-lg font-bold text-[#ec711e]">LET'S CREATE TOGETHER</p>
+              <h1 className="text-white text-5xl font-bold text-center">Ready To Start Your Project?</h1>
+              <h1 className="text-gray-100 text-2sm font-bold text-center  sm:w-90">Bring Your ideas to life with a team that cares about your vision  Let's create something amazing together</h1>
+                <button type="button" className=" cursor-pointer sm:w-90 w-full text-white text-xl font-semibold h-12 rounded-md backdrop-blur-xl bg-linear-to-r from-[#eb640aa6] to-[#ec403b] hover:bg-[#f16d14a6] ">LET'S TALK  &#8594;</button>
+
+          </div>
+          {/* last section end */}
+          {/* footer start */}
+          <div className="w-full h-150 flex flex-col items-center justify-evenly px-3 gap-5 bg-[#0f1011] sm:flex sm:flex-row
+           sm:h-100 sm:items-center sm:justify-center lg:items-normal lg:justify-normal
+          ">
+             <div className="w-full h-full bg-amber flex flex-col items-center sm:mt-20  ">
+                <div className="flex flex-row gap-2 items-center-safe justify-center sm:items-baseline sm:justify-start w-40 h-15 "> <img src={logo} className="w-7" /> <p className="text-white font-semibold text-xl">Giants</p> </div>
+                <p className="text-gray-100 text-2sm font-bold text-center sm:text-left">We design We Edit We Elevate brands. 
+                  Turning creative ideas into powerfull visual for modern businesses
+                </p>
+                <h2 className="text-sm text-gray-300 font-mono font-semibold mt-15 lg:mt-23">Copyright@2026 <span className="">Gaints</span> .All right reserved.</h2>
+              </div> 
+
+              <div className="w-full h-full bg-amber flex flex-col items-center sm:mt-20">
+                <div className="flex flex-row items-ceter justify-center gap-5">
+                  <Link to="#" className="min-[20px]:block min-[1024px]:block lg:hover:border-b-3 lg:rounded-md lg:active:border-b-2 lg:active:rounded-2xl lg:transiton lg:duration-100 lg:linear lg:border-white lg:h-8 lg:w-20 lg:text-sm lg:flex items-center lg:justify-center  lg:font-bold">Home</Link>
+                  <Link to="#" className="min-[20px]:block min-[1024px]:block lg:hover:border-b-3 lg:rounded-md lg:active:border-b-2 lg:active:rounded-2xl lg:transiton lg:duration-100 lg:linear lg:border-white lg:h-8 lg:w-20 lg:text-sm lg:flex items-center lg:justify-center  lg:font-bold">Our Work</Link>
+                  <Link to="#" className="min-[20px]:block min-[1024px]:block lg:hover:border-b-3 lg:rounded-md lg:active:border-b-2 lg:active:rounded-2xl lg:transiton lg:duration-100 lg:linear lg:border-white lg:h-8 lg:w-20 lg:text-sm lg:flex items-center lg:justify-center  lg:font-bold">About Us</Link>
+                  <Link to="#" className="min-[20px]:block min-[1024px]:block lg:hover:border-b-3 lg:rounded-md lg:active:border-b-2 lg:active:rounded-2xl lg:transiton lg:duration-100 lg:linear lg:border-white lg:h-8 lg:w-20 lg:text-sm lg:flex items-center lg:justify-center  lg:font-bold">LET'S TALK</Link>
+
+                </div>
+                <div className="flex flex-row items-center justify-evenly gap-5 sm:flex sm:flex-row mt-35 *:">
+                  <img src={facebook} className="w-25 sm:w-15 md:w-15 rounded-full" />
+                  <img src={instgram} className="w-25 sm:w-15 md:w-15 rounded-full" />
+                  <img src={whatup} className="w-25   sm:w-15 md:w-15 rounded-full" />
+                  
+                  </div>
+              </div> 
+             
+          </div>
+          {/* footer end */}
         </div>
         
       </div>
